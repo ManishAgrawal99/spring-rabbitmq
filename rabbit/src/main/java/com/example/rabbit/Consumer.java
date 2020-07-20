bbd00a8ff3f9bf7570834fac4401b102fecdd7e0
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-07-20 16:09:58 +0530

    Created a Producer

---
+++ rabbit/src/main/java/com/example/rabbit/Consumer.java
@@ -0,0 +1,10 @@
+package com.example.rabbit;
+
+public class Consumer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- rabbit/src/main/java/com/example/rabbit/Publisher.java
+++ rabbit/src/main/java/com/example/rabbit/Publisher.java
@@ -1,12 +1,27 @@
 package com.example.rabbit;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 public class Publisher {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, TimeoutException {
 		
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.newConnection();
 		
+		Channel channel = connection.createChannel();
 		
+		String message = "First Message";
 		
+		channel.basicPublish("", "Queue-1", null, message.getBytes());
+		
+		channel.close();
+		connection.close();
 	}
 
 }
